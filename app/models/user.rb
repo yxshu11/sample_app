acde76323cb036788235fcb1476a952951ff19fe
Mac Shu <mac_shu11@hotmail.com>
2015-06-12 12:02:26 +0000

    Finish Exercise 8.6

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,17 +12,23 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
   # Returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
+  # def self.digest(string) #define the digest using self, which is same with User
   
-  # Returns a random token.
-  def User.new_token
-    SecureRandom.urlsafe_base64
-  end
+  class << self
+    
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+    
+    # Returns a random token.
+    # def self.new_token #define the token using self, which is same with User
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   
+  end
   # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
